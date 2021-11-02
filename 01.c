58a66bf8be782bb8b17d52b4e5e9f997cba1864f
Gonzalo <Gonzalo>
2021-11-02 11:59:51 -0300

    primer commit

--- 01.c
+++ 01.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+void print_array(int a[], int n) {
+  for (int i = 0; i < n; i++) {
+    printf("%d\n",a[i]);
+  }
+}
 
 int main(void){
-
+  int array[]={1,2,3,4,5,6};
+  print_array(array,6);
   return 0;
 }
