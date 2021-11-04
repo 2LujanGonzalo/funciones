bcedea521abf2754473cbb1e98d7f32a6c87742d
gonzalo <gonzalo>
2021-11-04 08:56:06 -0300

    1

--- 01.c
+++ 01.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-void print_array(int a[], int n) {
-  for (int i = 0; i < n; i++) {
-    printf("%d\n",a[i]);
-  }
+void saludar(){
+printf("Hola\n");
 }
 
+
+
+
 int main(void){
-  int array[]={1,2,3,4,5,6};
-  print_array(array,6);
+saludar();
+
   return 0;
 }
