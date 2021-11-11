5c87fc8652846d3ae493842e1d00561ba151fa71
gonzalo <gonzalo>
2021-11-11 11:00:51 -0300

    17

--- 18.c
+++ 18.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-int factorial(int a){
-while (a!=0) {
-  return factorial((a-1)*a);
-}
-}
 
+int factorial(int a) {
+  if (a == 0) return 1;
+  return factorial(a - 1) * a;
+}
 
 int main(int argc, char  *argv[]) {
   int a=atoi(argv[1]);
