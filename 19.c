55d38d24af2352c038e9511adcd9b9bd12fad366
gonzalo <gonzalo>
2021-11-16 11:41:47 -0300

    19

---
+++ 19.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void fibo(int n){
+
+int fibonacci[n];
+fibonacci[0]=0;
+fibonacci[1]=1;
+for (int i = 2; i < n; i++) {
+fibonacci[i]= fibonacci[i-1]+fibonacci[i-2];
+}
+for (int i = 0; i < n; i++) {
+  printf("%d ", fibonacci[i]);
+}
+printf("\n" );
+}
+
+int main(int argc, char  *argv[]) {
+int n=atoi(argv[1]);
+  fibo(n);
+  return 0;
+}
